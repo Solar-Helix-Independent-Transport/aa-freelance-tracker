1dfc53c283e348039046f731ddd4e45d42dfde8a
Aaron Kable <aaronkable@gmail.com>
2026-07-05 18:21:15 +0800

    fix URL's

--- freelance_tracker/__init__.py
+++ freelance_tracker/__init__.py
@@ -2,4 +2,4 @@
 
 __version__ = "0.0.1"
 __title__ = "Freelance Tracker"
-__url__ = "https://github.com/aaronkable/aa-freelance-tracker"
+__url__ = "https://github.com/Solar-Helix-Independent-Transport/aa-freelance-tracker"
